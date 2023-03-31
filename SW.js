ec3b86eeeedc5ed344054c81e90c8e0a7c2ed36f
Sutan <sutandami@gmail.com>
2023-03-31 08:16:57 +0700

    tambahkan offline cache

--- SW.js
+++ SW.js
@@ -1,35 +1,51 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
-            '/',
-            'index.html',
-            'styles.css',
-            'app.js',
-            'blog.html',
-            'about.html',
-            'contact.html',
-            'portfolio-example01.html'
-          ])
-        })
-    );
-    return self.clients.claim();
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("first-app").then(function (cache) {
+      cache.addAll([
+        "/",
+        "index.html",
+        "styles.css",
+        "app.js",
+        "blog.html",
+        "about.html",
+        "contact.html",
+        "portfolio-example01.html",
+      ]);
+    })
+  );
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.open('first-app')
-        .then(function(cache) {
-          return fetch(event.request)
-            .then(function(res) {
-              cache.put(event.request, res.clone());
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.open("first-app").then(function (cache) {
+      return fetch(event.request).then(function (res) {
+        cache.put(event.request, res.clone());
+        return res;
+      });
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open("first-app").then(function (cache) {
+              cache.put(event.request.url, res.clone());
               return res;
             });
-        })
-    );
-  });
-  
-  
-  
-  
+          })
+          .catch(function (err) {
+            return caches.open("first-app").then(function (cache) {
+              return cache.match("/offline.html");
+            });
+          });
+      }
+    })
+  );
+});
