3a249c9afe0ec81a48986a7ed9b5e94b415c6b2a
Sutan <sutandami@gmail.com>
2023-03-31 08:37:08 +0700

    cache revision again

--- SW.js
+++ SW.js
@@ -1,52 +1,29 @@
-// self.addEventListener("install", function (event) {
-//   event.waitUntil(
-//     caches.open("first-app").then(function (cache) {
-//       cache.addAll([
-//         "/",
-//         "index.html",
-//         "styles.css",
-//         "app.js",
-//         "blog.html",
-//         "about.html",
-//         "contact.html",
-//         "portfolio-example01.html",
-//       ]);
-//     })
-//   );
-//   return self.clients.claim();
-// });
-
-// self.addEventListener("fetch", function (event) {
-//   event.respondWith(
-//     caches.open("first-app").then(function (cache) {
-//       return fetch(event.request).then(function (res) {
-//         cache.put(event.request, res.clone());
-//         return res;
-//       });
-//     })
-//   );
-// });
-
-
-const CACHE_NAME = 'first-app-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/blog.html',
-  '/about.html',
-  '/contact.html',
-  '/portfolio-example01.html'
-];
-
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open("first-app").then(function (cache) {
+      cache.addAll([
+        "/",
+        "index.html",
+        "styles.css",
+        "app.js",
+        "blog.html",
+        "about.html",
+        "contact.html",
+        "portfolio-example01.html",
+      ]);
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.open("first-app").then(function (cache) {
+      return fetch(event.request).then(function (res) {
+        cache.put(event.request, res.clone());
+        return res;
+      });
+    })
   );
 });
 
@@ -54,43 +31,35 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        // Cache hit - return response
         if (response) {
           return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open('first-app')
+                .then(function(cache) {
+                  return fetch(event.request)
+                  .then(function(res){
+
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+          })
+            .catch(function(err) {
+              return caches.open('first-app')
+                .then(function(cache) {
+                  return fetch(event.request)
+                  .then(function(res){
+
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+                });
+            });
         }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-      .catch(function(error) {
-        // If both cache and network fail, return a fallback response.
-        return caches.match('/offline.html');
       })
   );
 });
+
 
